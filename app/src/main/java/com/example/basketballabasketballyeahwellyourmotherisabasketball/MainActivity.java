c1693dab998a5f69ef8fb4e40c60fad301aae5e1
MoistCockital <rugygejonator.yes@gmail.com>
2022-11-10 10:49:19 +0100

    Livedata fajnie

--- app/src/main/java/com/example/basketballabasketballyeahwellyourmotherisabasketball/MainActivity.java
+++ app/src/main/java/com/example/basketballabasketballyeahwellyourmotherisabasketball/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.basketballabasketballyeahwellyourmotherisabasketball;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +12,8 @@ import com.example.basketballabasketballyeahwellyourmotherisabasketball.databind
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private int punkty=0;
+    //private int punkty=0;
+    PunktyViewModel punktyViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +21,38 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        punktyViewModel = new ViewModelProvider(this).get(PunktyViewModel.class);
+        punktyViewModel.getPunkty().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                binding.punktytextView.setText(String.valueOf(integer));
+            }
+        });
 
         binding.btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                punkty++;
-                        binding.punktytextView.setText(String.valueOf(punkty));
+                //punkty++;
+                punktyViewModel.dodajPunkty(1);
             }
         });
+
+        binding.btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                //punkty++;
+                punktyViewModel.dodajPunkty(2);
+            }
+        });
+
+        binding.btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                //punkty++;
+                punktyViewModel.dodajPunkty(3);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/basketballabasketballyeahwellyourmotherisabasketball/PunktyViewModel.java
@@ -0,0 +1,26 @@
+package com.example.basketballabasketballyeahwellyourmotherisabasketball;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PunktyViewModel extends ViewModel {
+    private MutableLiveData<Integer> punkty;
+
+    public MutableLiveData<Integer> getPunkty() {
+        if(punkty == null){
+            punkty = new MutableLiveData<>();
+            punkty.setValue(0);
+        }
+        return punkty;
+    }
+
+    public void setPunkty(int punkty) {
+        this.punkty.setValue(punkty);
+    }
+
+    public void dodajPunkty(int i){
+        if(punkty.getValue() != null){
+            punkty.setValue(punkty.getValue()+i);
+        }
+    }
+}
